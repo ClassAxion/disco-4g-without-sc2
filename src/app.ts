35d98cec08a9acf1b3fb82d735d999cd72c4495b
Damian (ClassAxion) <damian@techseo.pl>
2021-07-04 19:39:00 +0200

    more envs for config

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,9 @@ const startWithoutDisco: boolean = !!process.env.NO_DISCO;
 let disco: ParrotDisco = new ParrotDisco({
     debug: !!process.env.DEBUG,
     ip: process.env.DISCO_IP || '192.168.42.1',
+    streamControlPort: Number(process.env.STREAM_CONTROL_PORT || '55005'),
+    streamVideoPort: Number(process.env.STREAM_CONTROL_PORT || '55004'),
+    d2cPort: Number(process.env.D2C_PORT || '9988'),
 });
 
 let isConnected: boolean = false;
